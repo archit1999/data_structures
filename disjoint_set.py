00713eb696c2ade1d605e75aae4f8d1e0f7ccfda
Archit Krishna Mishra <archit@iiitkalyani.ac.in>
2020-10-25 22:02:55 +0530

    Update and rename exp234.py to disjoint_set.py

--- disjoint_set.py
+++ disjoint_set.py
@@ -2,13 +2,15 @@
 # author - Archit K Mishra
 
 
-def find_parent(u, parent):
+def find_parent(u, parent):                             # find top most parent of the given vertex
     while parent[u] != u:
         u = parent[u]
     return u
 
 
-def merge(u, v, parent, rank):
+def merge(u, v, parent, rank):                          # add two roots of the tree into a single tree
+    if u == v:
+        return
     if rank[u] < rank[v]:
         parent[u] = v
     if rank[v] < rank[u]:
@@ -18,26 +20,16 @@ def merge(u, v, parent, rank):
         rank[v] += 1
 
 
-def enter_edge(u, v, parent, rank):
+def enter_edge(u, v, parent, rank):                     # add given vertex wrt to edge to suitable sets
     merge(find_parent(u, parent), find_parent(v, parent), parent, rank)
-
-
-def same_parent(u, v, parent):
-    if find_parent(u, parent) == find_parent(v, parent):
-        return 1
-    else:
-        return 0
 
 
 v, edges = map(int, input().split())                    # no. of vertices and edges
 parent = [i for i in range(v)]                          # parent of ith vertex zero indexed
-rank = [1 for i in range(v)]                            # rank of ith vertex zero indexed
+rank = [1 for i in range(v)]                            # height of ith vertex zero indexed
 
 for i in range(edges):
     u, v = map(int, input().split())
     enter_edge(u-1, v-1, parent, rank)
 
 # print(parent, rank)
-
-u, v = map(int, input().split())
-print(same_parent(u-1, v-1, parent))
